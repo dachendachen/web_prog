a44027f16b81d96ddeb805907fe7addc71f94a94
Chen <ninodachen@yahoo.com>
2020-03-03 11:39:49 -0500

    finished the basic outline and technical rollover parts

---
+++ hw3/chenWeb.js
@@ -0,0 +1,38 @@
+window.onload = rolloverInit;
+
+function rolloverInit() {
+	for (var i=0; i<document.images.length; i++) {
+		if (document.images[i].parentNode.tagName == "A") {
+			setupRollover(document.images[i]);
+		}
+	}
+}
+
+function setupRollover(thisImage) {
+	thisImage.outImage = new Image();
+	thisImage.outImage.src = thisImage.src;
+	thisImage.onmouseout = rollOut;
+
+	thisImage.clickImage = new Image();
+	thisImage.clickImage.src = "buttons/" + thisImage.id + "2.gif";
+	thisImage.onclick = rollClick;	
+
+	thisImage.overImage = new Image();
+	thisImage.overImage.src = "buttons/" + thisImage.id + "3.gif";
+	thisImage.onmouseover = rollOver;	
+}
+
+function rollOver() {
+	this.src = this.overImage.src;
+}
+
+function rollOut() {
+	this.src = this.outImage.src;
+}
+
+function rollClick() {
+	this.src = this.clickImage.src;
+}
+
+						
+
